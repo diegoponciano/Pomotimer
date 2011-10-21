88a1fd583993ff552314ea216efa768db3b4f43b
Diego <diego@diego-pinguyos.(none)>
2011-10-21 13:05:51 -0200

    cleanup test

--- test/pomodorolist_test.py
+++ test/pomodorolist_test.py
@@ -4,13 +4,6 @@ import os
 import sys
 import unittest
 
-path = os.path.abspath(os.path.dirname(__file__))
-src_dir = os.path.join(path,os.pardir, 'src')
-os.chdir(src_dir)
-
-if src_dir not in sys.path:
-    sys.path.insert(0, src_dir)
-
 import mainwidget
 
 class PomodoroListTest(unittest.TestCase):
